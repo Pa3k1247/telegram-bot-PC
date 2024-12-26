538891943964ad34547c6bc6601582a31460dd9f
Pa3k1247 <121031759+Pa3k1247@users.noreply.github.com>
2024-12-26 12:43:09 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -37,15 +37,12 @@ app = Flask(__name__)
 
 REGISTERED_DEVICES = {}
 
-@app.route("/register_device", methods=["POST"])
-def register_device():
+@app.route('/webhook', methods=['POST'])
+def webhook():
     data = request.json
-    mac_address = data.get("mac_address")
-    ip_address = data.get("ip_address")
-    if mac_address and ip_address:
-        REGISTERED_DEVICES[mac_address] = ip_address
-        return jsonify({"message": "Устройство зарегистрировано."}), 200
-    return jsonify({"message": "Некорректные данные."}), 400
+    print("Received data:", data)
+    return {"status": "ok"}, 200
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
